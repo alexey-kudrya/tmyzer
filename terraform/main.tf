f1e9a1bcaf8cd9fd2a6e17565fa94abec8f78c48
Alexey Kudrya <aleksej.kudrja@gmail.com>
2022-11-21 01:00:14 +0200

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,39 @@
+resource "aws_dynamodb_table" "users-dynamodb-table" {
+  name           = "TmyzerUsers"
+  billing_mode   = var.table_billing_mode
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  tags = {
+    Name    = "tmyzer-users-table"
+    Project = "tmyzer"
+  }
+}
+
+resource "aws_instance" "tmyzer" {
+  ami             = data.aws_ami.amazon-linux-2.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.tmyzer.name]
+  key_name        = var.key_name
+
+  user_data       = <<-EOF
+                   #!/bin/bash
+                   python3 -m pip install --upgrade pip
+                   python3 -m pip install --upgrade telethon
+                   EOF
+
+  iam_instance_profile = aws_iam_instance_profile.tmyzer_ec2_profile.name
+
+  tags = {
+    Name        = "tmyzer"
+    Project     = "tmyzer"
+  }
+}
+
 resource "aws_security_group" "tmyzer" {
   name = "tmyzer"
 
@@ -14,22 +50,6 @@ resource "aws_security_group" "tmyzer" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_dynamodb_table" "users-dynamodb-table" {
-  name           = "TmyzerUsers"
-  billing_mode   = var.table_billing_mode
-  hash_key       = "UserId"
-
-  attribute {
-    name = "UserId"
-    type = "S"
-  }
-
-  tags = {
-    Name    = "tmyzer-users-table"
-    Project = "tmyzer"
   }
 }
 
@@ -77,24 +97,4 @@ resource "aws_iam_role_policy_attachment" "tmyzer_ec2_dynamodb_attach" {
 resource "aws_iam_instance_profile" "tmyzer_ec2_profile" {
   name = "tmazer_ec2_profile"
   role = aws_iam_role.tmyzer_ec2_role.name
-}
-
-resource "aws_instance" "tmyzer" {
-  ami             = data.aws_ami.amazon-linux-2.id
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.tmyzer.name]
-  key_name        = var.key_name
-
-  user_data       = <<-EOF
-                   #!/bin/bash
-                   python3 -m pip install --upgrade pip
-                   python3 -m pip install --upgrade telethon
-                   EOF
-
-  iam_instance_profile = aws_iam_instance_profile.tmyzer_ec2_profile.name
-
-  tags = {
-    Name        = "tmyzer"
-    Project     = "tmyzer"
-  }
 }
